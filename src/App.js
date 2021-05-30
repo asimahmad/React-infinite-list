9f377992a09d620709d30e791e1973fad1358673
Asim <asim005011997@gmail.com>
2021-05-30 13:13:16 +0530

    Create react app which displays the list of book for infinte until the data ends

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, {useState, useRef, useCallback} from 'react'
+import {useBookSearch} from './useBookSearch'
+import {Container, Grid, Card, CardContent, makeStyles, TextField, List} from '@material-ui/core'
 function App() {
+  const classes = useStyle();
+  const [query, setQuery] = useState('')
+  const [pageNumber, setPageNumber] = useState(1)
+  const {books, hasMore, loading, error} = useBookSearch(query, pageNumber)
+
+
+  const observe = useRef()
+  const lastBookItemRef = useCallback(node =>{
+    if(loading) return
+    if(observe.current) observe.current.disconnect()
+    observe.current = new IntersectionObserver(entries =>{
+      if(entries[0].isIntersecting && hasMore){
+        setPageNumber(prevPageNumber => prevPageNumber + 1)
+        console.log('Visible')
+      }
+    })
+    if(node) observe.current.observe(node)
+    //console.log(node)
+  }, [loading, hasMore])
+  
+  function handleSearch(e){
+    // console.log("Books",books)
+    setQuery(e.target.value)
+    setPageNumber(1)
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Container className={classes.useStyle}>
+       <Card>
+       <h2 className={classes.title}>Search Books</h2>
+         <CardContent>
+           <Grid container spacing={2}>
+             <Grid item xl={4} lg={4} md={6} sm={12} xs={12}>
+             <TextField type="text" value={query} label="Search book" onChange={handleSearch} />
+                {books.map((book, index) =>{
+                  if(books.length === index+1){
+                    return <div ref={lastBookItemRef} key={book}>{book}</div>
+                  }
+                  return <div key={book}>{book}</div>
+                })}
+                <div>{loading && `Loading...`}</div>
+                <div>{error && 'Error'}</div>
+             </Grid>
+             </Grid>
+          </CardContent>
+        </Card>
+        </Container>
+    </>
   );
 }
+
+const useStyle = makeStyles((theme) =>({
+  container: {
+    marginTop: 30,
+  },
+  title:{
+    display:'flex',
+    justifyItems: 'center',
+    textAlign: 'center',
+    background: '#0A8E78',
+    color: '#fff',
+    padding: 20
+  }
+}))
 
 export default App;
